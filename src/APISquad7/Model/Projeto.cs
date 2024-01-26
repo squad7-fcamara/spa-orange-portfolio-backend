6725dfb50c125d94aec8423f63a05735d29c99ef
ediltonx <ediltonx@hotmail.com>
2024-01-26 12:04:33 -0300

    feature de projetos adicionada

---
+++ src/APISquad7/Model/Projeto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APISquad7.Model
+{
+    public class Projeto
+    {
+        public Projeto() { }
+        public Projeto(string titulo, string imagem, string tag, string link, string descricao)
+        {
+            Titulo = titulo;
+            Imagem = imagem;
+            Tag = tag;
+            Link = link;
+            Descricao = descricao
+        }
+
+        public int IdProjeto { get; set; }
+        public string Titulo { get; set; }
+        public string Imagem { get; set; }
+        public string Tag { get; set; }
+        public string Link { get; set; }
+        public string Descricao { get; set; }
+    }
+}
